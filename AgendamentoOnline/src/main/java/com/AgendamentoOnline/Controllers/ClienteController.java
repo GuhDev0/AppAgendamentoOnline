362505d4cf130ecdb8500a55c9b4e5903b384eed
DevGustavo <gustavo.costaaraujo027@mail.com>
2025-07-24 12:11:14 -0300

    eidPoint RegistroDoProfissional

--- AgendamentoOnline/src/main/java/com/AgendamentoOnline/Controllers/ClienteController.java
+++ AgendamentoOnline/src/main/java/com/AgendamentoOnline/Controllers/ClienteController.java
@@ -2,29 +2,34 @@ package com.AgendamentoOnline.Controllers;
 
 import com.AgendamentoOnline.Dtos.DtoAgendamento;
 import com.AgendamentoOnline.Dtos.DtoCliente;
+import com.AgendamentoOnline.Dtos.DtoProfissional;
 import com.AgendamentoOnline.Dtos.ResponseRequest;
 import com.AgendamentoOnline.Models.Agendamento;
 import com.AgendamentoOnline.Models.Cliente;
+import com.AgendamentoOnline.Models.Profissional;
 import com.AgendamentoOnline.Services.AgendamentoServices;
 import com.AgendamentoOnline.Services.ClienteServices;
+import com.AgendamentoOnline.Services.ProfissionalServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/Cliente")
+@RequestMapping("/agendamentoOnline")
 public class ClienteController {
 
     private final ClienteServices clienteServices;
     private final AgendamentoServices agendamentoServices;
+    private final ProfissionalServices profissionalServices;
 
-    public ClienteController(ClienteServices clienteServices, AgendamentoServices agendamentoServices) {
+    public ClienteController(ClienteServices clienteServices, AgendamentoServices agendamentoServices, ProfissionalServices profissionalServices) {
         this.clienteServices = clienteServices;
         this.agendamentoServices = agendamentoServices;
+        this.profissionalServices = profissionalServices;
     }
 
-    @PostMapping
+    @PostMapping("/registrarCliente")
     public ResponseEntity<Cliente> save(@RequestBody DtoCliente dtoCliente){
         return  ResponseEntity.status(HttpStatus.CREATED).body(clienteServices.saveRepository(dtoCliente));
     }
@@ -38,5 +43,10 @@ public class ClienteController {
     public ResponseEntity<ResponseRequest> saveAgendamento(@RequestBody DtoAgendamento dtoAgendamento){
         agendamentoServices.novoAgendamento(dtoAgendamento);
         return ResponseEntity.ok(new ResponseRequest("Agendamento Marcado","200"));
+    }
+
+    @PostMapping("/registerProfissional")
+    public ResponseEntity<Profissional> saveProfissional(@RequestBody DtoProfissional dtoProfissional){
+        return ResponseEntity.status(HttpStatus.CREATED).body(profissionalServices.saveProfissional(dtoProfissional));
     }
 }
